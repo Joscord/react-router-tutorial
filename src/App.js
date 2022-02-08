346ab19027b5873d37ebfdcae0c1b9fb6a6f4177
joscord <josecorderodev@gmail.com>
2022-02-08 15:05:07 -0300

    Modified App.js, Home.js. Created Article.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import About from './pages/About';
+import Article from './pages/Article';
 
 const App = () => {
 	return (
@@ -23,6 +24,8 @@ const App = () => {
 						<About />
 					</Route>
 					<Route path='/contact' component={Contact} />
+          {/* Nótese que el path es dinámico y para eso usamos : esto le dice a react-router que es un parámetro de ruta y que puede cambiar*/}
+          <Route path='/articles/:id' component={Article}/>
 				</Switch>
 			</BrowserRouter>
 		</div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { useFetch } from '../hooks/useFetch'
 import './Home.css'
 
@@ -14,6 +15,8 @@ const Home = () => {
         <div key={id} className='card'>
           <h3>{title}</h3>
           <p>{author}</p>
+          {/* Usamos un Link. Usamos un valor dinámico en la prop to, para pasar un id  */}
+          <Link to={`/articles/${id}`}>Read more...</Link>
         </div>
       ))}
     </div>
