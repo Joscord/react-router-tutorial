baa78cda2d6df0baad23ded67e77d1bee411fc6a
joscord <josecorderodev@gmail.com>
2022-02-08 15:20:33 -0300

    Modified Article.js

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,6 +1,25 @@
+// Importamos el hook useParams
+import { useParams } from 'react-router-dom';
+import { useFetch } from '../hooks/useFetch';
 
 const Article = () => {
-  return <div>Article</div>;
+	// Usamos el hook, este nos retorna un objeto prams con los parámetros de la Url de la página. Con esto podemos extraer el id (nótese que es id porque así lo llamados en la ruta)
+	const { id } = useParams();
+    // Usamos nuestro useFetch para obtener la data
+    const {data: article, isPending, error} = useFetch(`http://localhost:3000/articles/${id}`)
+	return (
+        <div>
+            {isPending && <p>Loading article...</p>}
+            {error && <p>{error}</p>}
+            {article && (
+                <div>
+                    <h2>{article.title}</h2>
+                    <p>By {article.author}</p>
+                    <p>{article.body}</p>
+                </div>
+            )}
+        </div>
+    )
 };
 
 export default Article;
