599b4bd949f767b1781e4236c5004f42490faacc
joscord <josecorderodev@gmail.com>
2022-02-08 16:16:43 -0300

    Modified Article.js adding useHistory

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,25 +1,38 @@
 // Importamos el hook useParams
-import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
 import { useFetch } from '../hooks/useFetch';
 
 const Article = () => {
-	// Usamos el hook, este nos retorna un objeto prams con los parámetros de la Url de la página. Con esto podemos extraer el id (nótese que es id porque así lo llamados en la ruta)
 	const { id } = useParams();
-    // Usamos nuestro useFetch para obtener la data
-    const {data: article, isPending, error} = useFetch(`http://localhost:3000/articles/${id}`)
+	const {
+		data: article,
+		isPending,
+		error,
+	} = useFetch(`http://localhost:3000/articles/${id}`);
+	// Para redirigir usamos otro hook, useHistory. useHistory nos retorna un objeto que podemos usar
+	const history = useHistory();
+
+	useEffect(() => {
+		// Revisamos si hay error y si lo hay redigirimos
+		if (error) {
+			// Hay diversos métodos para redirigir como .goBack que nos lleva un paso atrás en la historia de usuario (como presionar el botón de atrás en el navegador). En este caso usamos el método push que nos permite "empujar el usuario hace otra ruta". Redirigimos hacia la página home. Lo colocaremos dentro de un setTimeout para que el usuario tenga tiempo de ver el error. No olvidar que debemos añadir history como dependencia
+			setTimeout(() => history.push('/'), 2000);
+		}
+	}, [error, history]);
 	return (
-        <div>
-            {isPending && <p>Loading article...</p>}
-            {error && <p>{error}</p>}
-            {article && (
-                <div>
-                    <h2>{article.title}</h2>
-                    <p>By {article.author}</p>
-                    <p>{article.body}</p>
-                </div>
-            )}
-        </div>
-    )
+		<div>
+			{isPending && <p>Loading article...</p>}
+			{error && <p>{error}</p>}
+			{article && (
+				<div>
+					<h2>{article.title}</h2>
+					<p>By {article.author}</p>
+					<p>{article.body}</p>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default Article;
